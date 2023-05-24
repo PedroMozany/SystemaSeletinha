c8eff2c48c6f7bcb83832ec31c638b9d9db13381
Pedro Ramalho <pedroge70@gmail.com>
2023-05-24 17:34:48 -0300

    rota

--- seletinha/src/app/app-routing.module.ts
+++ seletinha/src/app/app-routing.module.ts
@@ -10,10 +10,12 @@ const routes: Routes = [{
   path: '',
   redirectTo: 'home',
   pathMatch: "full"
-},{
-  path: 'home',
-  component: HomeComponent
-},{
+},
+// {
+//   path: 'home',
+//   component: HomeComponent
+// },
+{
   path: 'cadastroAluno',
   component: CadAlunoComponent
 },{
